f407ffe29db0eb283d00dd371f917e98c8e5819e
Kavita Chaudhry <kchaudh312@gmail.com>
2025-11-15 11:39:32 -0700

    feat: setup shadcn

--- apps/living-kavita-loca/tailwind.config.ts
+++ apps/living-kavita-loca/tailwind.config.ts
@@ -1,12 +1,10 @@
 import type { Config } from "tailwindcss";
 
-
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../packages/ui-library/src/**/*.{js,ts,jsx,tsx,mdx}"
+    "../../packages/ui-library/src/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/shadcn-ui-lib/src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-
-
 };
 export default config;
---
+++ packages/shadcn-ui-lib/src/hooks/useBreakpoint.ts
@@ -0,0 +1,51 @@
+import * as React from "react";
+
+export function useIsMobile(mobileBreakpoint = 768) {
+  return useIsBreakpointMaxWidth(mobileBreakpoint);
+}
+
+export function useIsDesktopOrLarger(desktopBreakpoint = 1024) {
+  return useIsBreakpointMinWidth(desktopBreakpoint);
+}
+
+export function useIsDesktop(tabletBreakpoint = 768) {
+  const isLessThanDesktop = useIsBreakpointMaxWidth(1024);
+  return isLessThanDesktop;
+
+}
+
+export function useIsBreakpointMaxWidth(breakpoint = 768) {
+  const [isBreakpoint, setIsBreakpoint] = React.useState<boolean | undefined>(
+    undefined,
+  );
+
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${breakpoint - 1}px)`);
+    const onChange = () => {
+      setIsBreakpoint(window.innerWidth < breakpoint);
+    };
+    mql.addEventListener("change", onChange);
+    setIsBreakpoint(window.innerWidth < breakpoint);
+    return () => mql.removeEventListener("change", onChange);
+  }, [breakpoint]);
+
+  return !!isBreakpoint;
+}
+
+export function useIsBreakpointMinWidth(breakpoint = 768) {
+  const [isBreakpoint, setIsBreakpoint] = React.useState<boolean | undefined>(
+    undefined,
+  );
+
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(min-width: ${breakpoint}px)`);
+    const onChange = () => {
+      setIsBreakpoint(window.innerWidth >= breakpoint);
+    };
+    mql.addEventListener("change", onChange);
+    setIsBreakpoint(window.innerWidth >= breakpoint);
+    return () => mql.removeEventListener("change", onChange);
+  }, [breakpoint]);
+
+  return !!isBreakpoint;
+}
