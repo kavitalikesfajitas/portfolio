3ec2231b6b6ec6e696027bdc87089edb2dbe5203
Kavita Chaudhry <kchaudh312@gmail.com>
2025-11-18 10:57:29 -0700

    chore: convert into next image

--- apps/living-kavita-loca/app/main/Hero.tsx
+++ apps/living-kavita-loca/app/main/Hero.tsx
@@ -1,20 +1,25 @@
 "use client";
 import "./Hero.css";
+import Image from "next/image";
 
 export function Hero() {
   return (
     <section className="flex flex-col lg:items-center lg:justify-items-start  min-h-screen relative">
       <div className="relative inline-block scale-[0.6] md:scale-[0.5] lg:scale-75 lg:-mt-24 ">
-        <img
+        <Image
           src="/images/living-kavita-loca-logo.png"
           alt="living kavita loca"
+          height={1024}
+          width={1024}
           className="flex self-center"
         />
-        <div className="absolute -right-20 top-1/2 -translate-y-1/2">
-          <img
+        <div className="absolute -right-20 top-1/2 scale-50 -translate-y-1/2 translate-x-3/8">
+          <Image
             src="/images/lips-glossy.png"
             alt="lips open that are glossy"
-            className="lips relative z-10 scale-50 translate-x-3/8"
+            className="lips relative z-10"
+            height={1024}
+            width={1024}
           />
         </div>
       </div>
