5831cdd37a90c5609c3d73e853ab136bc7e0e9eb
Kavita Chaudhry <kchaudh312@gmail.com>
2026-06-02 10:55:33 -0600

    fix: lets be legal

--- packages/fonts/site/dammit-i-need-a-new-job/index.tsx
+++ packages/fonts/site/dammit-i-need-a-new-job/index.tsx
@@ -2,92 +2,13 @@ import localFont from "next/font/local";
 import { Inter } from "next/font/google";
 import clsx from "clsx";
 
-const fontHelveticaNeue = localFont({
-  src: [
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueUltraLight.otf",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueUltraLightItalic.otf",
-      weight: "200",
-      style: "italic",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueThin.otf",
-      weight: "250",
-      style: "normal",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueThinItalic.otf",
-      weight: "250",
-      style: "italic",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueLight.otf",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueLightItalic.otf",
-      weight: "300",
-      style: "italic",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueRoman.otf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueItalic.ttf",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueMedium.otf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueMediumItalic.otf",
-      weight: "500",
-      style: "italic",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueBold.otf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueBoldItalic.otf",
-      weight: "700",
-      style: "italic",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueHeavy.otf",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueHeavyItalic.otf",
-      weight: "800",
-      style: "italic",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueBlack.otf",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "../../fonts/helvetica-neue-5/HelveticaNeueBlackItalic.otf",
-      weight: "900",
-      style: "italic",
-    },
-  ],
+const fontInter = Inter({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "700", "800", "900"],
+  style: ["normal", "italic"],
   display: "swap",
   fallback: ["Helvetica", "Arial", "sans-serif"],
-  variable: "--font-helvetica-neue",
+  variable: "--font-inter",
 });
 
 const fontMidnightGelactic = localFont({
@@ -162,7 +83,7 @@ const fontLondrinaShadow = localFont({
 
 export function getAppFontVariablesForDammit() {
   return clsx(
-    fontHelveticaNeue.variable,
+    fontInter.variable,
     fontMidnightGelactic.variable,
     fontLobster.variable,
     fontOverpassMono.variable,
